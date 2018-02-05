26011bf19648a674cf6071c9e478499ef7d06e51
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2018-02-05 02:54:27 -0800

    add score field to board

--- src/board.rs
+++ src/board.rs
@@ -2,21 +2,27 @@ use std::collections::HashMap;
 
 enum Direction { Up, Down, Left, Right }
 
-type Board = [[u32; 4]; 4];
+#[derive(Debug)]
+struct Board { board: [[u32; 4]; 4], score: u32 }
+
+impl PartialEq for Board {
+    fn eq(&self, other: &Board) -> bool { self.board == other.board && self.score == other.score }
+}
 
 fn clone_board(b: &Board) -> Board {
     let mut dst = new_board();
-    dst.copy_from_slice(b);
+    dst.board.copy_from_slice(&b.board);
+    dst.score = b.score;
     dst
 }
 
-fn new_board() -> Board { return [[0; 4]; 4] }
+fn new_board() -> Board { return Board{ board: [[0; 4]; 4], score: 0 }}
 
 fn add_piece(b: &Board, get_pos: &Fn(&Board) -> Option<(usize, usize)>, get_val: &Fn() -> u32) -> Option<Board> {
     match get_pos(b) {
         Some((r, c)) => {
             let mut board = clone_board(b);
-            board[r][c] = get_val();
+            board.board[r][c] = get_val();
             Some(board)
         },
         None => None,
@@ -31,16 +37,17 @@ fn move_board(b: &Board, d: Direction) -> Option<Board> {
             for c in 0..4 {
                 let (mut free, mut last) = (0, 0);
                 for r in 0..4 {
-                    if board[r][c] != 0 {
-                        if r != last && board[r][c] == board[last][c] {
-                            board[last][c] *= 2;
-                            board[r][c] = 0;
+                    if board.board[r][c] != 0 {
+                        if r != last && board.board[r][c] == board.board[last][c] {
+                            board.board[last][c] *= 2;
+                            board.score += board.board[last][c];
+                            board.board[r][c] = 0;
                             last += 1;
                             modified = true;
                         } else if r != free {
-                            let tmp = board[free][c];
-                            board[free][c] = board[r][c];
-                            board[r][c] = tmp;
+                            let tmp = board.board[free][c];
+                            board.board[free][c] = board.board[r][c];
+                            board.board[r][c] = tmp;
                             modified = true;
                             free += 1;
                         } else {
@@ -54,16 +61,17 @@ fn move_board(b: &Board, d: Direction) -> Option<Board> {
             for c in 0..4 {
                 let (mut free, mut last) = (3, 3);
                 for r in (0..4).rev() {
-                    if board[r][c] != 0 {
-                        if r != last && board[r][c] == board[last][c] {
-                            board[last][c] *= 2;
-                            board[r][c] = 0;
+                    if board.board[r][c] != 0 {
+                        if r != last && board.board[r][c] == board.board[last][c] {
+                            board.board[last][c] *= 2;
+                            board.score += board.board[last][c];
+                            board.board[r][c] = 0;
                             last = if last > 0 { last - 1 } else { last }; // ?
                             modified = true;
                         } else if r != free {
-                            let tmp = board[free][c];
-                            board[free][c] = board[r][c];
-                            board[r][c] = tmp;
+                            let tmp = board.board[free][c];
+                            board.board[free][c] = board.board[r][c];
+                            board.board[r][c] = tmp;
                             modified = true;
                             free = if free > 0 { free - 1 } else { free }; // ?
                         } else {
@@ -77,16 +85,17 @@ fn move_board(b: &Board, d: Direction) -> Option<Board> {
             for r in 0..4 {
                 let (mut free, mut last) = (0, 0);
                 for c in 0..4 {
-                    if board[r][c] != 0 {
-                        if c != last && board[r][c] == board[r][last] {
-                            board[r][last] *= 2;
-                            board[r][c] = 0;
+                    if board.board[r][c] != 0 {
+                        if c != last && board.board[r][c] == board.board[r][last] {
+                            board.board[r][last] *= 2;
+                            board.score += board.board[r][last];
+                            board.board[r][c] = 0;
                             last += 1;
                             modified = true;
                         } else if c != free {
-                            let tmp = board[r][free];
-                            board[r][free] = board[r][c];
-                            board[r][c] = tmp;
+                            let tmp = board.board[r][free];
+                            board.board[r][free] = board.board[r][c];
+                            board.board[r][c] = tmp;
                             modified = true;
                             free += 1;
                         } else {
@@ -100,16 +109,17 @@ fn move_board(b: &Board, d: Direction) -> Option<Board> {
             for r in 0..4 {
                 let (mut free, mut last) = (3, 3);
                 for c in (0..4).rev() {
-                    if board[r][c] != 0 {
-                        if c != last && board[r][c] == board[r][last] {
-                            board[r][last] *= 2;
-                            board[r][c] = 0;
+                    if board.board[r][c] != 0 {
+                        if c != last && board.board[r][c] == board.board[r][last] {
+                            board.board[r][last] *= 2;
+                            board.score += board.board[r][last];
+                            board.board[r][c] = 0;
                             last  = if last > 0 { last - 1 } else { last }; // ?
                             modified = true;
                         } else if c != free {
-                            let tmp = board[r][free];
-                            board[r][free] = board[r][c];
-                            board[r][c] = tmp;
+                            let tmp = board.board[r][free];
+                            board.board[r][free] = board.board[r][c];
+                            board.board[r][c] = tmp;
                             modified = true;
                             free = if free > 0 { free - 1 } else { free }; // ?
                         } else {
@@ -128,7 +138,7 @@ fn new_val_2() -> u32 { 2 }
 fn low_pos(b: &Board) -> Option<(usize, usize)> {
     for r in 0 .. 4 {
         for c in 0 .. 4 {
-            if b[r][c] == 0 { return Some((r, c)); }
+            if b.board[r][c] == 0 { return Some((r, c)); }
         }
     }
     None
@@ -141,7 +151,7 @@ mod tests {
     #[test]
     fn test_new_board() {
         let b = new_board();
-        assert_eq!(b, [[0; 4]; 4])
+        assert_eq!(b, Board{ board:[[0; 4]; 4], score: 0 })
     }
 
     #[test]
@@ -149,15 +159,14 @@ mod tests {
         fn t(b: Board, r: Option<(usize, usize)>) { assert_eq!(low_pos(&b), r); }
 
         t(new_board(), Some((0, 0)));
-        t([[1; 4], [1; 4], [1; 4], [1, 1, 1, 0]], Some((3, 3)));
-        t([[1; 4]; 4], None);
+        t(Board{board:[[1; 4], [1; 4], [1; 4], [1, 1, 1, 0]], score: 0}, Some((3, 3)));
+        t(Board{board:[[1; 4]; 4], score: 0}, None);
     }
 
     #[test]
     fn test_add_piece() {
         let b = add_piece(&new_board(), &low_pos, &new_val_2);
-        assert!(b.is_some());
-        assert_eq!(b.unwrap(), [[2, 0, 0, 0], [0; 4], [0; 4], [0; 4]]);
+        assert_eq!(b, Some(Board{board:[[2, 0, 0, 0], [0; 4], [0; 4], [0; 4]], score: 0}));
     }
 
     #[test]
@@ -167,32 +176,32 @@ mod tests {
             assert_eq!(b, result);
         }
 
-        t(Direction::Left, [[0; 4]; 4], None);
-        t(Direction::Left, [[1, 0, 0, 0]; 4], None);
-        t(Direction::Left, [[1, 2, 3, 4]; 4], None);
-        t(Direction::Left, [[0, 0, 0, 1]; 4], Some([[1, 0, 0, 0]; 4]));
-        t(Direction::Left, [[1, 0, 0, 1]; 4], Some([[2, 0, 0, 0]; 4]));
-        t(Direction::Left, [[1, 1, 2, 2]; 4], Some([[2, 4, 0, 0]; 4]));
+        t(Direction::Left, new_board(), None);
+        t(Direction::Left, Board{board: [[1, 0, 0, 0]; 4], score: 0}, None);
+        t(Direction::Left, Board{board: [[1, 2, 3, 4]; 4], score: 0}, None);
+        t(Direction::Left, Board{board: [[0, 0, 0, 1]; 4], score: 0}, Some(Board{board: [[1, 0, 0, 0]; 4], score: 0}));
+        t(Direction::Left, Board{board: [[1, 0, 0, 1]; 4], score: 0}, Some(Board{board: [[2, 0, 0, 0]; 4], score: 8}));
+        t(Direction::Left, Board{board: [[1, 1, 2, 2]; 4], score: 0}, Some(Board{board: [[2, 4, 0, 0]; 4], score: 24}));
 
-        t(Direction::Right, [[0;4]; 4], None);
-        t(Direction::Right, [[0, 0, 0, 1]; 4], None);
-        t(Direction::Right, [[1, 2, 3, 4]; 4], None);
-        t(Direction::Right, [[1, 0, 0, 0]; 4], Some([[0, 0, 0, 1]; 4]));
-        t(Direction::Right, [[1, 0, 0, 1]; 4], Some([[0, 0, 0, 2]; 4]));
-        t(Direction::Right, [[1, 1, 2, 2]; 4], Some([[0, 0, 2, 4]; 4]));
+        t(Direction::Right, new_board(), None);
+        t(Direction::Right, Board{board: [[0, 0, 0, 1]; 4], score: 0}, None);
+        t(Direction::Right, Board{board: [[1, 2, 3, 4]; 4], score: 0}, None);
+        t(Direction::Right, Board{board: [[1, 0, 0, 0]; 4], score: 0}, Some(Board{board: [[0, 0, 0, 1]; 4], score: 0}));
+        t(Direction::Right, Board{board: [[1, 0, 0, 1]; 4], score: 0}, Some(Board{board: [[0, 0, 0, 2]; 4], score: 8}));
+        t(Direction::Right, Board{board: [[1, 1, 2, 2]; 4], score: 0}, Some(Board{board: [[0, 0, 2, 4]; 4], score: 24}));
 
-        t(Direction::Up, [[0;4]; 4], None);
-        t(Direction::Up, [[1; 4], [0; 4], [0; 4], [0; 4]], None);
-        t(Direction::Up, [[1; 4], [2; 4], [3; 4], [4; 4]], None);
-        t(Direction::Up, [[0; 4], [0; 4], [0; 4], [1; 4]], Some([[1; 4], [0; 4], [0; 4], [0; 4]]));
-        t(Direction::Up, [[1; 4], [0; 4], [0; 4], [1; 4]], Some([[2; 4], [0; 4], [0; 4], [0; 4]]));
-        t(Direction::Up, [[1; 4], [1; 4], [2; 4], [2; 4]], Some([[2; 4], [4; 4], [0; 4], [0; 4]]));
+        t(Direction::Up, new_board(), None);
+        t(Direction::Up, Board{board: [[1; 4], [0; 4], [0; 4], [0; 4]], score: 0}, None);
+        t(Direction::Up, Board{board: [[1; 4], [2; 4], [3; 4], [4; 4]], score: 0}, None);
+        t(Direction::Up, Board{board: [[0; 4], [0; 4], [0; 4], [1; 4]], score: 0}, Some(Board{board: [[1; 4], [0; 4], [0; 4], [0; 4]], score: 0}));
+        t(Direction::Up, Board{board: [[1; 4], [0; 4], [0; 4], [1; 4]], score: 0}, Some(Board{board: [[2; 4], [0; 4], [0; 4], [0; 4]], score: 8}));
+        t(Direction::Up, Board{board: [[1; 4], [1; 4], [2; 4], [2; 4]], score: 0}, Some(Board{board: [[2; 4], [4; 4], [0; 4], [0; 4]], score: 24}));
 
-        t(Direction::Down, [[0;4]; 4], None);
-        t(Direction::Down, [[0; 4], [0; 4], [0; 4], [1; 4]], None);
-        t(Direction::Down, [[1; 4], [2; 4], [3; 4], [4; 4]], None);
-        t(Direction::Down, [[1; 4], [0; 4], [0; 4], [0; 4]], Some([[0; 4], [0; 4], [0; 4], [1; 4]]));
-        t(Direction::Down, [[1; 4], [0; 4], [0; 4], [1; 4]], Some([[0; 4], [0; 4], [0; 4], [2; 4]]));
-        t(Direction::Down, [[1; 4], [1; 4], [2; 4], [2; 4]], Some([[0; 4], [0; 4], [2; 4], [4; 4]]));
+        t(Direction::Down, new_board(), None);
+        t(Direction::Down, Board{board: [[0; 4], [0; 4], [0; 4], [1; 4]], score: 0}, None);
+        t(Direction::Down, Board{board: [[1; 4], [2; 4], [3; 4], [4; 4]], score: 0}, None);
+        t(Direction::Down, Board{board: [[1; 4], [0; 4], [0; 4], [0; 4]], score: 0}, Some(Board{board: [[0; 4], [0; 4], [0; 4], [1; 4]], score: 0}));
+        t(Direction::Down, Board{board: [[1; 4], [0; 4], [0; 4], [1; 4]], score: 0}, Some(Board{board: [[0; 4], [0; 4], [0; 4], [2; 4]], score: 8}));
+        t(Direction::Down, Board{board: [[1; 4], [1; 4], [2; 4], [2; 4]], score: 0}, Some(Board{board: [[0; 4], [0; 4], [2; 4], [4; 4]], score: 24}));
     }
 }
